b367e477d4e6c447ec4886b8c745456f4edf4fa2
Gal Kerem <gal.kerem@gmail.com>
2024-06-01 12:58:52 +0300

    feat: Add dotenv support for secure API key handling
    - Integrated `python-dotenv` to load environment variables from a .env file.
    - Updated `demo_gradio.py` to read the OpenAI API key from the .env file.
    - Specified `gradio` version 4.32 in `requirements_demo.txt`.
    - Included `python-dotenv` in `requirements_demo.txt` for managing environment variables.
    
    This enhances security by avoiding hardcoding API keys in the script.

--- tests/demos/demo_gradio.py
+++ tests/demos/demo_gradio.py
@@ -2,12 +2,23 @@ import sys
 
 import gradio as gr
 
+from dotenv import load_dotenv
+import os
+
 sys.path.insert(0, './agency-swarm')
 
 from agency_swarm import set_openai_key, Agent
-
 from agency_swarm.agency.agency import Agency
 from agency_swarm.tools.oai import FileSearch, CodeInterpreter
+
+# Load the environment variables from the .env file
+load_dotenv()
+
+# Get the OpenAI API key from the environment variable
+api_key = os.getenv("OPENAI_API_KEY")
+
+# Set the OpenAI API key
+set_openai_key(api_key)
 
 ceo = Agent(name="CEO",
             description="Responsible for client communication, task planning and management.",
